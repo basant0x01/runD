ac89acc632d21f1c86e2204d9c19ae0334cbc8b1
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2023-12-28 22:10:46 +0545

    Update runD.sh

--- runD.sh
+++ runD.sh
@@ -4,29 +4,39 @@
 TARGET=""
 
 
-function damage(){
-
-
-
-}
-
-
-function nuke(){
-
-
-
-}
-
-
 function Unleash(){
 
+# HTTP: 80
+# HTTPS: 443
+# FTP: 21
+# SSH: 22
+# TELNET: 23
+# SMTP: 25
+# DNS 53
+# POP3: 110
+# SFTP: 115
+# IMAP: 143
+# MSRPC: 135
+# NETBIOS: 137
+# NETBIOS: 139
+# SMB: 445
+# MYSQL: 3306
+# MYSQL: 1433
+# RDP: 3389
 
+my_ports=("80" "21" "22" "23")
+
+for ports in "${my_ports[@]}"; do
+    masscan -iL runD/hosts.txt -p $ports 2>/dev/null | tee > runD/all_open_ports.txt
+done
 
 }
 
-
 function rampage(){
-
+echo "Running: fping"
+mkdir runD
+fping -a -g $TARGET 2>/dev/null | tee > runD/all_hosts.txt
+Unleash
 }
 
 
@@ -52,10 +62,10 @@ function mainScreen() {
     echo "Rampage Unleash Nuke and Damage"
     echo "-------------------------------"
     echo -e "Selected Target: "$TARGET
+    rampage
 }
 
 # Main execution starts here
-mainScreen
 userInput
 
 # Now you can use the $TARGET variable for further processing
