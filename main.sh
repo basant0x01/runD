258d07459a95a67796a22e6609b3b446edac5430
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2024-10-06 16:46:31 +0545

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,29 +4,35 @@
 # Usage: Network Scanner & Exploiter
 
 # Tool Requirements:
-    # fping
-    # masscan
-    # WebShot
-    # Httpx
+# fping
+# masscan
+# WebShot
+# Httpx
 
 # GLOBAL VARIABLES
 TARGET=""
-TIME_TAKING_WARRNING="It may take time, so grab your coffee!"
+TIME_TAKING_WARNING="It may take time, so grab your coffee!"
 TASK_COMPLETED="The script has completed the task - runD"
 
-function task_completed(){
+# Function to check for root privileges
+function check_root() {
+    if [ "$EUID" -ne 0 ]; then
+        echo "This script must be run as root. Please use 'sudo' or run as root user."
+        exit 1
+    fi
+}
+
+function task_completed() {
     echo -e "$TASK_COMPLETED"
 }
 
-function rampage_on_port80(){
-# Indicate that the task has been completed
-bash exploit_port_80.sh
-task_completed
-
+function rampage_on_port80() {
+    bash exploit_port_80.sh
+    task_completed
 }
 
-function rampage(){
-: << 'RAMPAGE'
+function rampage() {
+    : << 'RAMPAGE'
 [ a course of violent, riotous, or reckless action or behavior ]
 Rampage (R): In the context of a network scanner and exploiter, "Rampage" 
 could refer to a scanning phase where the tool aggressively looks for vulnerabilities and weaknesses 
@@ -34,65 +40,46 @@ in the target network. This might involve port scanning, service enumeration,
 and identifying potential entry points.
 RAMPAGE
 
-echo "Running: fping"
-mkdir runD
-fping -a -g $TARGET 2>/dev/null | tee > runD/all_hosts.txt
+    echo "Running: fping"
+    mkdir -p runD
+    fping -a -g $TARGET 2>/dev/null | tee > runD/all_hosts.txt
 
-# HTTP: 80
-# HTTPS: 443
-# FTP: 21
-# SSH: 22
-# TELNET: 23
-# SMTP: 25
-# DNS 53
-# POP3: 110
-# SFTP: 115
-# IMAP: 143
-# MSRPC: 135
-# NETBIOS: 137
-# NETBIOS: 139
-# SMB: 445
-# MYSQL: 3306
-# MYSQL: 1433
-# RDP: 3389
-# ... Add more ports as needed
-echo "Running: masscan"
+    echo "Running: masscan"
 
-my_ports=("80") #"21" "22" "23" Open ports according to your requirements
+    my_ports=("80") # "21" "22" "23" Open ports according to your requirements
 
-# Create a folder to store open ports files
-mkdir -p runD/open_juicy_ports
+    # Create a folder to store open ports files
+    mkdir -p runD/open_juicy_ports
 
-for port in "${my_ports[@]}"; do
-    output_file="runD/open_port${port}.txt"
-    ips_file="runD/ips_port${port}.txt"
-    port_folder="runD/open_juicy_ports/$port"
-    
-    # Run masscan and save the output to the specified file
-    masscan -iL runD/all_hosts.txt -p $port --output-format grepable --output-filename "$output_file" > /dev/null 2>&1
+    for port in "${my_ports[@]}"; do
+        output_file="runD/open_port${port}.txt"
+        ips_file="runD/ips_port${port}.txt"
+        port_folder="runD/open_juicy_ports/$port"
 
-    # Extract and save IPs using grep and awk
-    cat "$output_file" | grep -o 'Host:.*' | awk '{print $2}' > "$ips_file"
+        # Run masscan and save the output to the specified file
+        masscan -iL runD/all_hosts.txt -p $port --output-format grepable --output-filename "$output_file" > /dev/null 2>&1
 
-    # Create a folder for the port if it doesn't exist
-    mkdir -p "$port_folder"
+        # Extract and save IPs using grep and awk
+        grep -o 'Host:.*' "$output_file" | awk '{print $2}' > "$ips_file"
 
-    # Move files to the respective folders
-    mv "$ips_file" "$port_folder/"
+        # Create a folder for the port if it doesn't exist
+        mkdir -p "$port_folder"
 
-    # Delete the previous open_port${port}.txt file
-    rm -f "$output_file"
-done
+        # Move files to the respective folders
+        mv "$ips_file" "$port_folder/"
 
-rampage_on_port80
+        # Delete the previous open_port${port}.txt file
+        rm -f "$output_file"
+    done
 
+    rampage_on_port80
 }
 
 function userInput() {
     clear
     read -p "Enter your Target (Example: *.*.*.*/*): " ip
 
-    # validate CIDR using regex
+    # Validate CIDR using regex
     if [[ $ip =~ ^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/([0-9]+)$ ]]; then
         TARGET="$ip"
         echo "Valid input. Target: $TARGET"
@@ -111,11 +98,12 @@ function mainScreen() {
     echo "###################################################"
     echo "#----- Scripted/Developed - by basant0x01 --------#"
     echo "###################################################"
-    echo -e "Selected Target: "$TARGET
+    echo -e "Selected Target: $TARGET"
     rampage
 }
 
 # Main execution starts here
+check_root  # Check for root privileges
 userInput
 # ----------------------------------------
 # NOTE: "IT MAY ACT LIKE A NETWORK PT BOT"
